8b6f4f3ed94d340bf95d4adf089907d3274c87de
liutku5 <l1utkuz20@gmail.com>
2024-05-24 11:52:26 +0300

    sukurtos knygos

---
+++ src/Book.java
@@ -0,0 +1,41 @@
+public class Book {
+    private String title;
+    private int pages;
+    private long releaseYear;
+    public Book(){
+
+    }
+    public Book(String title, int pages, long releaseYear) {
+        this.title = title;
+        this.pages = pages;
+        this.releaseYear = releaseYear;
+    }
+
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.getTitle();
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPages() {
+        return this.getPages();
+    }
+    public void setReleaseYear(long releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getReleaseYear() {
+        return this.getReleaseYear();
+    }
+}
+
+//        sukurti 3 objektus su reikšmėmis naudojantis tuščiu konstruktoriu, ir tris naudojantis pilnu konstuktoriu.
+//sudėti juos į knygų masyvą.
+//prasukti ciklą per masyvą ir atspausdinti knygas (naudojam getterius)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Book book = new Book();
+        book.setTitle("The Prisoner's Throne");
+        book.setPages(368);
+        book.setReleaseYear(2024-04-05);
+        Book book1 = new Book();
+        book1.setTitle("People of the Storm");
+        book1.setPages(284);
+        book1.setReleaseYear(2022-04-28);
+        Book book2 = new Book();
+        book2.setTitle("Landis");
+        book2.setPages(332);
+        book2.setReleaseYear(2023-11-7);
+        Book book3 = new Book("The Enchanted Castle",255,2019-06-13);
+        Book book4 = new Book("The Legend of Black Jack",456,2022-05-15);
+        Book book5 = new Book("Grey Lore",388,2019-08-06);
+
+
+
+
+
     }
+
 }
+//
+//sudėti juos į knygų masyvą.
+//prasukti ciklą per masyvą ir atspausdinti knygas (naudojam getterius)
